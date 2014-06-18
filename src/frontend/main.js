8927aa6a1d7d567a596f716129f9a6d14654a176
Owen Lynch <oclynch888@gmail.com>
2014-06-17 22:26:26 -0400

    hello world main.js

---
+++ src/frontend/main.js
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+var React = require('react');
+var socket = require('socket.io');
+
+React.renderComponent(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
